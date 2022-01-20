52e42cd56225d89204d99bd32f562a513b12c211
Sigit <sigittt53@gmail.com>
2022-01-20 10:30:23 +0700

    update form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,24 @@ import Header from './layout/Header';
 
 function Form() {
   const navigate = useNavigate()
+
+  const handleUpload = () => {
+    alert('asd')
+  }
+
   return <>
     <Header />
     <div className="container mt-5 p-3">
       <div className="row justify-content-center">
         <div className="col-lg-6">
+          <p className='text-center fw-bold'>Please upload your image with our product</p>
           <form action="">
             <div className="form-group">
               <label htmlFor="image">Upload Image</label>
               <input type="file" name="image" id="image" className='form-control mt-3' />
             </div>
             <div className="button-wrapper">
-              <button className="btn primary me-2">Upload</button>
+              <button className="btn primary me-2" type='button' onClick={() => handleUpload()}>Upload</button>
               <button className="btn secondary" onClick={() => navigate('/transaction')}>Back</button>
             </div>
           </form>
